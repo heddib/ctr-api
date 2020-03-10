f63aba54bf536a923fd44f1434d8111a446b0a38
Kanna <heddi932@gmail.com>
2020-03-10 17:25:01 +0100

    Nice titre

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -8,7 +8,7 @@ return [
         |--------------------------------------------------------------------------
         */
 
-        'title' => 'API Documatation',
+        'title' => 'API Documentation',
     ],
 
     'routes' => [
